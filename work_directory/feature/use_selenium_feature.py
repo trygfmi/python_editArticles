36faa114bb239f50e015f71a34f7c605f6522b65
running-terminal-commands-blog <running-terminal-commands-blog@running-terminal-commands-blog.com>
2025-10-23 14:47:04 +0900

    chromeを使用するための処理と、編集後に保存ボタンを押す処理を追加

--- work_directory/feature/use_selenium_feature.py
+++ work_directory/feature/use_selenium_feature.py
@@ -6,6 +6,9 @@ import pyperclip
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from work_directory.feature.read_env import *
 
 
 sleep_time_number=0.4
@@ -42,7 +45,6 @@ def press_something_block_print(driver, search_string):
 def open_edit_articles(driver, search_string):
     # if(WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, search_string)))):
     if(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, search_string)))):
-        print("要素が見つかりました")
         press_something_block_elements=driver.find_elements(By.CSS_SELECTOR, search_string)
 
         for element in press_something_block_elements:
@@ -101,3 +103,32 @@ def get_element_by_id(driver, search_string):
         print("要素が見つかりませんでした")
         exit(1)
         
+def getDriver():
+    chrome_options = Options()
+    # ユーザーデータディレクトリとプロファイルを指定
+    chrome_options.add_argument("--user-data-dir="+user_data_dir)
+    chrome_options.add_argument("--profile="+profile)
+
+    # 初めてseleniumからchromeを起動してログインする時に使用
+    # ボット検知を回避するための追加オプション
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # WebDriverの自動化フラグを無効化
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])  # 自動化の通知を非表示
+
+    return webdriver.Chrome(options=chrome_options)
+
+def confirm_save(driver, window_handles):
+    for i in range(1,len(window_handles)):
+        print(i)
+        driver.switch_to.window(window_handles[i])
+        title = driver.title
+        print(title)
+        
+        print("保存する場合はokを入力してenterを押してください。しない場合はenterを押すだけで大丈夫です")
+        key=input()
+        if(key == "ok"):
+            print("保存しました")
+            press_something_block(driver, '[class="components-button editor-post-publish-button editor-post-publish-button__button is-primary is-compact"]')
+
+        else:
+            print("保存されませんでした")
+            
